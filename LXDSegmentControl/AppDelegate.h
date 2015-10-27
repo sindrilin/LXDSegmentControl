31f8c05d705f6ce34c571c9ff52bfc3fd80ceb35
Sindri Lin <634893985@qq.com>
2015-10-27 09:07:57 +0800

    Initial Commit

---
+++ LXDSegmentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXDSegmentControl
+//
+//  Created by 林欣达 on 15/10/27.
+//  Copyright © 2015年 LXD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXDSegmentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXDSegmentControl
+//
+//  Created by 林欣达 on 15/10/27.
+//  Copyright © 2015年 LXD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
